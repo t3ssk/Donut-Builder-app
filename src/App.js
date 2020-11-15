4293f5511146b62dc783d753bcbff6b145f38952
t3ssk <te.konecna@icloud.com>
2020-11-16 00:12:08 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Donuti<i class="fa fa-circle"></i></h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
